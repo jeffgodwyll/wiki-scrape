0a50c53cdb1eb618996ff94ac878132f96d1df42
Jeffrey Godwyll <jeffgodwyll@gmail.com>
2016-10-23 18:55:04 +0000

    Parse wiki article and grab links

--- main.py
+++ main.py
@@ -1,5 +1,15 @@
+import urlparse
+import logging
+
 import tornado.web
+
 import tornado.wsgi
+
+import requests
+import requests_toolbelt.adapters.appengine
+
+# make requests use urlfetch
+requests_toolbelt.adapters.appengine.monkeypatch()
 
 TEMPLATE = """
 <html>
@@ -16,13 +26,63 @@ TEMPLATE = """
 </html>
 """
 
+# logger
+logger = logging.getLogger(__name__)
+
+
+def get_sheet_id(url):
+    path = urlparse.urlsplit(url).path
+    try:
+        sheet_id = path.split('/')[3]
+    except IndexError:
+        logger.exception('url passed is {}'.format(url))
+        sheet_id = ''
+    return sheet_id
+
+
+def is_url():
+    # TODO: validate url
+    pass
+
+
+def get_article(url):
+    # TODO: validate url
+    try:
+        article = requests.get(url)
+        return article.text
+    except requests.exceptions.RequestException as e:
+        logger.error(e)
+
+
+def get_links(article):
+    links = []
+    start_tag = '<a href="/wiki'
+    end_tag = '">'
+    for text in article.split("</a>"):
+        if "<a href" in text:
+            try:
+                ind = text.index(start_tag)
+                text = text[ind+len(start_tag):]
+                end = text.index(end_tag)
+            except:
+                pass
+            else:
+                link = text[:end]
+                a = link.split('title')
+                logger.info(a)
+                links.append(text[:end])
+    return links
+
 
 class MainHandler(tornado.web.RequestHandler):
     def get(self):
         self.write(TEMPLATE)
 
     def post(self):
-        pass
+        url = self.get_argument('article')
+        article_text = get_article(url)
+        links = get_links(article_text)
+        self.write(''.join(links))
 
 
 app = tornado.web.Application([
