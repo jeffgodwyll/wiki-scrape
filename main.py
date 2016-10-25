bb0ddbb1f661c0888ebb50a51d9005f1d68013d3
Jeffrey Godwyll <jeffgodwyll@gmail.com>
2016-10-25 06:27:31 +0000

    Implements app engine datastore counter for existing urls
    Creates entity to test for uniqueness.
    Some pep 8 fixes

--- main.py
+++ main.py
@@ -3,15 +3,15 @@ from collections import defaultdict
 
 import json
 
-import tornado.web
+from google.appengine.ext import ndb
 
+import tornado.web
 import tornado.wsgi
+
+from webapp2_extras.appengine.auth.models import Unique
 
 import requests
 import requests_toolbelt.adapters.appengine
-
-from google.appengine.ext import ndb
-# from webapp2_extras.appengine.auth.models import Unique
 
 # make requests use urlfetch
 requests_toolbelt.adapters.appengine.monkeypatch()
@@ -44,9 +44,24 @@ class Article(ndb.Model):
     url = ndb.StringProperty()
     # title = ndb.StringProperty(required=False)
     scraped = ndb.JsonProperty()
+    count = ndb.IntegerProperty(default=0)
 
-    # TODO: Check for existing article and increase count
-    # use https://github.com/jeffgodwyll/qod-api/blob/master/main.py#L50 as base
+
+def get_count():
+    '''value counter'''
+    total = 0
+    for counter in Article.query():
+        total += counter
+    return total
+
+
+# TODO: implement actual shard?
+@ndb.transactional
+def increment(id):
+    """Increment counter value"""
+    counter = Article.get_by_id(id)
+    counter.count += 1
+    counter.put()
 
 
 def tree():
@@ -111,9 +126,7 @@ class MainHandler(tornado.web.RequestHandler):
 
     def get(self):
         self.write(TEMPLATE)
-        self.write('<br>')
-        self.write('<br>')
-        self.write('<hr>')
+        self.write('<br><hr><br>')
         articles = Article.query().fetch()
         self.write(str(articles))
 
@@ -122,6 +135,7 @@ class MainHandler(tornado.web.RequestHandler):
         '''
         l = 6
         url = self.get_argument('article')
+
         article_text = get_article(url)
         links = get_links(article_text)
 
@@ -131,14 +145,6 @@ class MainHandler(tornado.web.RequestHandler):
         # first 6 links
         for i, l1 in enumerate(links[:l]):
 
-            '''self.write('<br>')
-            self.write('<br>')
-            self.write('* ' + l1[0])
-            self.write('<br>')'''
-
-            # check if url has been scraped already and increase count in model
-            # entity use ff transaction as base:
-            # https://github.com/jeffgodwyll/qod-api/blob/master/main.py#L50
             l1_url = BASE_URL % l1[0]
             link_text = get_article(l1_url)
             child_links = get_links(link_text)
@@ -149,18 +155,34 @@ class MainHandler(tornado.web.RequestHandler):
 
                 for i3, l3 in enumerate(child_links2[:l]):
 
-                    scraped['root'][url][i]['url'][l1[0]][i2]['url'][l2[0]][i3]['url'] = l3[0]
-                    scraped['root'][url][i]['url'][l1[0]][i2]['url'][l2[0]][i3]['title'] = l3[1]
-                    scraped['root'][url][i]['url'][l1[0]][i2]['title'] = l2[1]
-                    scraped['root'][url][i]['title'] = l1[1]
+                    root = scraped['root'][url][i]
+
+                    root['url'][l1[0]][i2]['url'][l2[0]][i3]['url'] = l3[0]
+                    root['url'][l1[0]][i2]['url'][l2[0]][i3]['title'] = l3[1]
+                    root['url'][l1[0]][i2]['title'] = l2[1]
+                    root['title'] = l1[1]
 
         self.write('<pre>{}</pre>'.format(
             json.dumps(scraped, sort_keys=True, indent=2)))
-        article = Article(
-            url=url,
-            scraped=json.loads(json.dumps(scraped))
-        )
-        article.put()
+
+        uniques = ['Article.url.%s' % url, ]
+        success, existing = Unique.create_multi(uniques)
+
+        if success:
+
+            logger.info('New article being scraped: {}'.format(url))
+            article = Article(
+                url=url,
+                scraped=json.loads(json.dumps(scraped))
+            )
+            article.put()
+        else:
+            logger.info('property %r not unique' % existing)
+            article = Article.query(Article.url == url).get()
+            article_id = article.key.id()
+            increment(article_id)
+            logger.info('counter incremented')
+            print article
 
 
 app = tornado.web.Application([
